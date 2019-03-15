9f4a26234c79164b3a22949fa62238a27149e9d4
inesbarradas <inesmartinsbarradas1996@gmail.com>
2019-03-15 12:38:47 +0000

    Resolução do exercicio 4 para entender as strings

---
+++ PercorreString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variaveis
+            string s, cStr;
+            char c;
+
+            //Pedir ao utilizador a string 
+            Console.WriteLine("Insira a string que pretende:");
+            s = Console.ReadLine();
+
+            //Pedir ao utilizador o caracter
+            Console.WriteLine("Insira o char:");
+            cStr = Console.ReadLine();
+
+            //Converter uma string num char
+            c = cStr[0];
+                       
+            //Imprimir string
+            //Precorrer a string
+            foreach(char cInString in s)
+                //char actual da string diferentes ao char do user?
+                if (cInString != c)
+                {
+                    //imprimir char actual da string
+                    Console.Write(cInString);
+
+                }
+
+            Console.Write("\n");
+
+
+        }
+    }
+}
