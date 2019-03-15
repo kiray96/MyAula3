c1375b1f96cad1851b073a5d98fffe7146b71426
inesbarradas <inesmartinsbarradas1996@gmail.com>
2019-03-15 11:02:39 +0000

    Primeira pergunta ao sistema

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -6,7 +6,32 @@ namespace ChatBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Variáveis auxiliares
+            string question, response;
+
+            //Para fazer uma pergunta ao utilizador
+            Console.WriteLine("Qual é a sua pergunta?");
+            question = Console.ReadLine();
+
+            //Escrever as perguntas que podem ser respondidas
+            switch(question)
+            {
+                case "Como estás?":
+                    response = "Estou bem obrigada";
+                    break;
+
+                default:
+                    response = "Não entendo por favor faça outra pergunta! :)";
+                    break;              
+
+            }
+
+
+            //Respostas
+            Console.WriteLine(response);
+
+
+
         }
     }
 }
